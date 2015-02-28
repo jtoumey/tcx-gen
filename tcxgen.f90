09099bc21ec2ab274aac54a2488cbd78f8d7ebcb
jtoumey <jtoumey@gmail.com>
2015-02-27 18:26:53 -0600

    sketched outline of script

--- tcxgen.f90
+++ tcxgen.f90
@@ -1,9 +1,32 @@
+!**************************************************************************!
+!                                                                          !
+!  Module:       TCXGEN.F90                                                !
+!                                                                          !
+!  Programmer:   Julian M. Toumey                                          !
+!                Madison, WI                                               !
+!                                                                          !
+!  Date:         February 2015                                             !
+!                                                                          !
+!  Language:     FORTRAN90                                                 !
+!                                                                          !
+!  Description:  This file generate *.tcx files based on user inputs.      !
+!                                                                          !
+!**************************************************************************!
 PROGRAM TCXGEN
 
+get input
+-average speed
+-average heart rate
+
+
+write main details
 
 
 
-
-
+do while
+   loop over time points
+   use pseudo-random numbers to add noise
+   
+   
 
 END

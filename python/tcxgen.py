f849de9ac943e19187fa618dd0bab59446cabb64
Julian T <jtoumey@gmail.com>
2015-03-12 23:02:25 -0500

    added a test file open and file print statement

--- python/tcxgen.py
+++ python/tcxgen.py
@@ -1,3 +1,20 @@
+from __future__ import print_function
 
-fname = input('Enter a file name to generate: ')
-print('Filename is: ',fname)
+
+# 
+# Query user for filename
+#
+fname = raw_input('Enter a file name to generate: ')
+print("Filename is: ",fname,'.tcx',sep='')
+
+# concatenate file name and extension
+exten = '.tcx'
+fname = fname + exten
+# open file to write
+f = open(fname,'w')
+
+
+
+f.write('Test\n')
+
+f.close()
